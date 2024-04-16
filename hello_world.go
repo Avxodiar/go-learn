114a629185f255d584e1f3d9935b170a672bcb5e
KHaritov Andrei <avxodiar@gmail.com>
2024-04-16 23:43:34 +0300

    added hello world

---
+++ hello_world.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    fmt.Println("Are you ready? (yes/no)")
+
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    ready := scanner.Text()
+
+    fmt.Println()
+    if ready == "yes" {
+        fmt.Println("Let`s GO!")
+    } else {
+        fmt.Println("You are not Prepared!")
+    }
+}
